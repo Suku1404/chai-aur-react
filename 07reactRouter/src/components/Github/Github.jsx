02f87ea2078719fdfb727b47479c34b15e906e92
Sakshi <sakshirajput1404@gmail.com>
2026-02-23 16:02:15 +0530

    react router

---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState }from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github() {
+    const data = useLoaderData()
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/suku-1404/followers')
+    //         .then(res => res.json())
+    //         .then(data => {console.log(data);
+    //         setData(data)
+    //     })
+    // }, [])
+
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white text-3xl p-4'>Github followers: {data.followers}
+    <img src={data.avatar_url} alt="Git profile" width={300} /></div>
+  )
+}
+
+export default Github
+
+export const githubInfoLoader = async () =>{
+    const response = await fetch('https://api.github.com/users/suku-1404/followers')
+    return response.json()
+
+}
